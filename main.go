8836f542f72b0db3620fd57596118fcf34cbfcd5
Renan Andrade <renan.andrade@formstack.com>
2022-09-20 22:56:18 -0300

    feat: scan folder and subfolder

--- main.go
+++ main.go
@@ -1,17 +1,53 @@
 package main
 
-import "path"
-
-type File struct{}
-type Folder struct{}
+import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	if isFolder("dtoFolder/testDto.json") {
+	providedPath := "dtoFolder"
+	filesToParse := []fs.FileInfo{}
 
+	if isSingleFile(providedPath) {
+		fileInfo, _ := os.Lstat(providedPath)
+		filesToParse = append(filesToParse, fileInfo)
+	} else {
+		filesToParse = scanFolder(providedPath)
 	}
 }
 
-func isFolder(filePath string) bool {
-	extension := path.Ext(filePath)
-	return len(extension) == 0
+func scanFolder(folderPath string) []fs.FileInfo {
+	files, _ := ioutil.ReadDir(folderPath)
+	returnFiles := []fs.FileInfo{}
+	folderToScan := []fs.FileInfo{}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			fmt.Println(file.Name())
+			returnFiles = append(returnFiles, file)
+		} else {
+			folderToScan = append(folderToScan, file)
+		}
+	}
+
+	for _, folder := range folderToScan {
+		newFolderPath := filepath.Join(folderPath, folder.Name())
+		scanFolder(newFolderPath)
+	}
+
+	return returnFiles
+}
+
+func isSingleFile(filePath string) bool {
+	fileInfo, err := os.Lstat(filePath)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return !fileInfo.IsDir()
 }
